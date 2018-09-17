99efbe922b7db98895bb410a5c8bbac61fc8fc3c
Eva van Rooden <evavanrooden@gmail.com>
2018-09-17 16:23:35 +0200

    added Contestant class

---
+++ src/main/java/com/capgemini/HungerGamesDemo/models/Contestant.java
@@ -0,0 +1,50 @@
+package com.capgemini.HungerGamesDemo.models;
+
+public class Contestant {
+
+    private boolean isFemale;
+    private boolean isAlive;
+    private int attack;
+    private int defense;
+    private int health;
+
+    public boolean isFemale() {
+        return isFemale;
+    }
+
+    public void setFemale(boolean female) {
+        isFemale = female;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
